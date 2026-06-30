327590676b1d7e9ffef08a485630bdeb25b5d01a
Ana <dutranacarolina@gmail.com>
2026-06-29 21:42:40 -0300

    testes

--- src/main/modules/stock/controllers/products.controller.spec.ts
+++ src/main/modules/stock/controllers/products.controller.spec.ts
@@ -5,20 +5,83 @@ import { ProductsService } from '../services/products.service';
 describe('ProductsController', () => {
   let controller: ProductsController;
 
-  beforeAll(() => {
-    const mockService = {
-      listProducts: vi.fn().mockResolvedValue([{ id: 1, name: 'Teste' }]),
-    } as unknown as ProductsService;
+  const mockService = {
+    listProducts:vi.fn(),
+    findProductById: vi.fn(),
+    createProduct: vi.fn(),
+    updateProduct: vi.fn(),
+    deleteProduct: vi.fn(),
+  } 
 
-    controller = new ProductsController(mockService);
+  beforeAll(() => {
+    controller = new ProductsController(mockService as unknown as ProductsService);
   });
 
   beforeEach(() => {
-    vi.restoreAllMocks();
+    vi.clearAllMocks();
   });
 
   it('deve retornar a lista de produtos', async () => {
+    const fakeProducts = [{ id: '1', name: 'Teste'}];
+    
+    mockService.listProducts.mockResolvedValue(fakeProducts);
+
     const result = await controller.index();
-    expect(result).toEqual([{ id: 1, name: 'Teste' }]);
+
+    expect(mockService.listProducts).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fakeProducts);
   });
+
+  it('deve retornar um produto pelo ID', async  () => {
+    const fakeProduct = {
+      id: '1',
+      name: 'Teste',
+    };
+
+    mockService.findProductById.mockResolvedValue(fakeProduct);
+
+    const result = await controller.find('1');
+
+    expect(mockService.findProductById).toHaveBeenCalledWith('1');
+    expect(result).toEqual(fakeProduct);
+  });
+
+  it('deve criar um produto', async () => {
+    const data = {
+      name: 'Notebook',
+      price: 3000,
+    }; 
+    
+    mockService.createProduct.mockResolvedValue(data);
+
+    const result = await controller.create(data);
+
+    expect(mockService.createProduct).toHaveBeenCalledWith(data);
+    expect(result).toEqual(data);
+  }); 
+  
+it('deve lançar erro quando o nome não for informado', async () => {
+  await expect(
+    controller.create({
+      name: '',
+      sku: '123',
+      quantity: 10,
+      price: 3000,
+      sellPrice: 3500,
+    })
+  ).rejects.toThrow('Nome obrigatório');
+});
+
+it('deve lançar erro quando o preço for invalido', async () => {
+  await expect(
+    controller.create({
+      name: 'Notebook',
+      sku: '123',
+      quantity: 10,
+      price: 0,
+      sellPrice: 3500,
+    })
+  ).rejects.toThrow('Preço inválido');
+});
+
 });
--- src/main/modules/stock/services/products.service.spec.ts
+++ src/main/modules/stock/services/products.service.spec.ts
@@ -1,26 +1,137 @@
 import { describe, it, expect, vi, beforeEach, beforeAll } from 'vitest';
-import { Repository } from 'typeorm';
+import { Equal, Repository } from 'typeorm';
 import { ProductsService } from './products.service';
 import { Product } from '../models/product.model';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
-  beforeAll(() => {
-    const mockRepository = {
-      find: vi.fn().mockResolvedValue([{ id: 1, name: 'Teste' }]),
-    } as unknown as Repository<Product>;
+  const mockRepository = {
+    find: vi.fn(),
+    findOne: vi.fn(),
+    create: vi.fn(),
+    save: vi.fn(),
+    update: vi.fn(),
+    delete: vi.fn(),
+  } as unknown as any;
 
+  beforeAll(() => {
     service = new ProductsService(mockRepository);
   });
 
   beforeEach(() => {
-    vi.restoreAllMocks();
+    vi.clearAllMocks();
   });
 
-  it('deve retornar a lista de produtos', async () => {
-    const result = await service.listProducts();
-    expect(result).toEqual([{ id: 1, name: 'Teste' }]);
+it('deve retornar a lista de produtos', async () => {
+  const fakeProducts = [
+    { id: '1', name: 'Teste' },
+  ];
+
+  mockRepository.find.mockResolvedValue(fakeProducts);
+
+  const result = await service.listProducts();
+
+  expect(mockRepository.find).toHaveBeenCalledTimes(1);
+  expect(result).toEqual(fakeProducts);
+});
+
+it ('deve retornar um produto pelo ID', async () => {
+  const fakeProduct = {
+    id: '1',
+    name: 'Teste'
+  };
+
+  mockRepository.findOne.mockResolvedValue(fakeProduct);
+
+  const result = await service.findProductById('1');
+
+  expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
+  expect(result).toEqual(fakeProduct);
+
+
+});
+
+it('deve lançar erro quando o produto não existir', async () => {
+  mockRepository.findOne.mockResolvedValue(null);
+
+  await expect(
+    service.findProductById('999')
+  ).rejects.toThrow('Produto não encontrado');
+
+  expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
+
+});
+
+it('deve criar um novo produto', async () => {
+  const data = {
+    name: 'Notebook',
+    sku: 'NB001',
+    quantity: 10,
+    price: 3000,
+    sellPrice: 3500,
+  };
+
+  mockRepository.create.mockReturnValue(data);
+  mockRepository.save.mockResolvedValue(data);
+
+  const result = await service.createProduct(data);
+
+  expect(mockRepository.create).toHaveBeenCalledWith(data);
+  expect(mockRepository.save).toHaveBeenCalled();
+  expect(result).toEqual(data);
+});
+
+it('deve atualizar um produto', async () => {
+  const product = {
+    id: '1',
+    name: 'Notebook',
+    sku: 'NB001',
+    quantity: 10,
+    price: 3000,
+    sellPrice: 3500,
+  };
+
+  const updateProduct = {
+    ...product,
+    price: 3200,
+  };
+  
+  mockRepository.findOne 
+   .mockResolvedValueOnce(product)
+   .mockResolvedValueOnce(updateProduct)
+
+  mockRepository.update.mockResolvedValue({ affected: 1});
+
+  const result = await service.updateProduct('1', {
+    price: 3200,
   });
+
+  expect(mockRepository.update).toHaveBeenCalledWith('1', {
+    price: 3200,  
+  });
+
+  expect(result).toEqual(updateProduct);
+});
+
+
+it('deve excluir um produto', async () => {
+  const product = {
+    id: '1',
+    name: 'Notebook',
+    sku: 'NB001',
+    quantity: 10,
+    price: 3000,
+    sellPrice: 3500,
+  };
+
+  mockRepository.findOne.mockResolvedValue(product);
+  mockRepository.delete.mockResolvedValue({ affected: 1});
+
+  const result = await service.deleteProduct('1');
+
+  expect(mockRepository.delete).toHaveBeenCalledWith('1');
+  expect(result).toEqual({success: true});
+})
 
 });
